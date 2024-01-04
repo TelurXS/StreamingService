8f77d3f5638e29241aa8eaea673e263c989082f2
TelurXS <vacaroslav@gmail.com>
2024-01-04 18:58:45 +0200

    Working on Authentication

---
+++ API/Endpoints/IdentityEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using Carter;
+
+namespace API.Endpoints;
+
+public class IdentityEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/identity", GetIdentityName).RequireAuthorization();
+    }
+
+    private async Task<IResult> GetIdentityName(ClaimsPrincipal claims)
+    {
+        return Results.Ok(claims.Claims);
+    }
+}
---
+++ API/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace API.Middlewares;
+
+public sealed class ExceptionHandler : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+            var response = new
+            {
+                Message = e.Message 
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
