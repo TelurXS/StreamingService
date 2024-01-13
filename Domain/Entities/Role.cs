7d4f0caf6a89339dd976d06d61ce4a021788fb92
TelurXS <vacaroslav@gmail.com>
2024-01-13 13:32:16 +0200

    Added Blazor Client

---
+++ Domain/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public class Role : IdentityRole<Guid>
+{
+	public static string User => nameof(User);
+	public static string Moderator => nameof(Moderator);
+	public static string Admin => nameof(Admin);
+}
