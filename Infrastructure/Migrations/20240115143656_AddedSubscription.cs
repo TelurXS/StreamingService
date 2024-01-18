7d94cbedce0c97294eee866b37fbb3ca02d46aea
TelurXS <vacaroslav@gmail.com>
2024-01-18 14:44:55 +0200

    Web Application redesign

---
+++ Infrastructure/Migrations/20240115143656_AddedSubscription.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rates_Accounts_AuthorId",
+                table: "Rates");
+
+            migrationBuilder.DropTable(
+                name: "LocalizedDescriptions");
+
+            migrationBuilder.DropTable(
+                name: "LocalizedNames");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AgeRestriction",
+                table: "Titles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "AvarageRate",
+                table: "Titles",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cast",
+                table: "Titles",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Country",
+                table: "Titles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Titles",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "Rates",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubscriptionExpiresIn",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SubscriptionId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comments_Comments_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Comments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comments_Titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Descriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Descriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Descriptions_Titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Names",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Names", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Names_Titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_AccountId",
+                table: "Rates",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_SubscriptionId",
+                table: "AspNetUsers",
+                column: "SubscriptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId",
+                table: "Comments",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ParentId",
+                table: "Comments",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_TitleId",
+                table: "Comments",
+                column: "TitleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Descriptions_TitleId",
+                table: "Descriptions",
+                column: "TitleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Names_TitleId",
+                table: "Names",
+                column: "TitleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Subscriptions_SubscriptionId",
+                table: "AspNetUsers",
+                column: "SubscriptionId",
+                principalTable: "Subscriptions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rates_Accounts_AccountId",
+                table: "Rates",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rates_AspNetUsers_AuthorId",
+                table: "Rates",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Subscriptions_SubscriptionId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rates_Accounts_AccountId",
+                table: "Rates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rates_AspNetUsers_AuthorId",
+                table: "Rates");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Descriptions");
+
+            migrationBuilder.DropTable(
+                name: "Names");
+
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rates_AccountId",
+                table: "Rates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_SubscriptionId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AgeRestriction",
+                table: "Titles");
+
+            migrationBuilder.DropColumn(
+                name: "AvarageRate",
+                table: "Titles");
+
+            migrationBuilder.DropColumn(
+                name: "Cast",
+                table: "Titles");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Titles");
+
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Titles");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Rates");
+
+            migrationBuilder.DropColumn(
+                name: "SubscriptionExpiresIn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SubscriptionId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "LocalizedDescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalizedDescriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LocalizedDescriptions_Titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocalizedNames",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalizedNames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LocalizedNames_Titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "Titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocalizedDescriptions_TitleId",
+                table: "LocalizedDescriptions",
+                column: "TitleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocalizedNames_TitleId",
+                table: "LocalizedNames",
+                column: "TitleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rates_Accounts_AuthorId",
+                table: "Rates",
+                column: "AuthorId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
