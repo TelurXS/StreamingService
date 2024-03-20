06d3e6649e4571be483e35b5a36fc8308032af85
TelurXS <vacaroslav@gmail.com>
2024-03-20 12:35:39 +0200

    Fixed New And Popular Page

--- Web/wwwroot/js/video-player.js
+++ Web/wwwroot/js/video-player.js
@@ -72,8 +72,6 @@ window.initializePlayer = (dotnet) => {
 
     const play = (notifyChanged = true) => {
 
-        console.log(`play incoked with parameters video.paused = ${video.paused}`)
-
         if (video.paused == false)
             return;
 
@@ -281,7 +279,6 @@ window.initializePlayer = (dotnet) => {
     }
 
     window.play = (notifyChanged = true) => {
-        console.log("Play invoked in js")
         play(notifyChanged);
     }
 
